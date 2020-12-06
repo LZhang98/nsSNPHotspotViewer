1f7b350f4e78bf7df1641fab81d905082bc4e326
LZhang98 <luke.taotao@hotmail.com>
2020-12-05 21:12:45 -0500

    Updated plotPopulation graph titles

--- R/plotPopulation.R
+++ R/plotPopulation.R
@@ -2,7 +2,7 @@
 #' by the dataset.
 #'
 #' Outputs a scatterplot using ggplot2 that plots the length of each gene against
-#' the number of SNPs observed on that gene. By default, each position that contains a SNP
+#' the number of SNPs observed on that gene. By default, each locus
 #' is counted once and only once -- repeats are ignored. However, this can be changed using
 #' the removeRepeats parameter.
 #'
@@ -22,12 +22,14 @@
 #' @export
 plotPopulation <- function(posData, removeRepeats=TRUE) {
 
-    library(ggplot2)
-
     # produce SNP count data. removeRepeats argument is used here.
     countsData <- getSNPCounts(posData, removeRepeats)
 
     # build dataframe for scatterplot, then output
     plotData <- data.frame(posData$transcript_id, geneLength=posData$gene_length,counts=countsData$counts)
-    ggplot2::ggplot(data=plotData) + geom_point(mapping = aes(x = geneLength, y = counts))
+    plot <- ggplot2::ggplot(data=plotData) + geom_point(mapping = aes(x = geneLength, y = counts))
+    plot <- plot + ggplot2::ggtitle("SNP Counts Across the Population") +
+        ggplot2::xlab("Gene Length") + ggplot2::ylab("Number of SNPs observed")
+    plot <- plot + ggplot2::theme(plot.title = element_text(hjust = 0.5))
+    plot
 }
