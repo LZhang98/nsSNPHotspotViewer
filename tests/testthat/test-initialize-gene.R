abf6b51867f1574f8f723e71db03741d678ec8a1
LZhang98 <luke.taotao@hotmail.com>
2020-12-09 13:21:37 -0500

    Gene initialization testing

---
+++ tests/testthat/test-initialize-gene.R
@@ -0,0 +1,17 @@
+test_that("gene intialization works", {
+    example <- "Potri.001G000800.1.v3.0"
+    gene <- initializeGene(example, snpPositions)
+
+    expect_equal(class(gene), "Gene")
+    expect_equal(gene$id, example)
+    expect_equal(gene$length, 3415)
+    expect_equal(dim(gene$posTable), c(5, 2))
+})
+
+test_that("missing data", {
+    example <- "asdf"
+    gene <-
+
+    expect_error(initializeGene(example, snpPositions),
+                 'Given id not found in dataset')
+})
--- tests/testthat/test-mean-spacing.R
+++ tests/testthat/test-mean-spacing.R
@@ -14,3 +14,9 @@ test_that("diff works", {
     my_set <- c(1, 4, 6, 9)
     expect_equal(diff(my_set), c(3,2,3))
 })
+
+test_that("padding works", {
+    example <- "Potri.002G203800.1.v3.0"
+    gene <- initializeGene(example, snpPositions)
+    expect_equal(gene$meanSpacing, (gene$length + 1)/2)
+})
